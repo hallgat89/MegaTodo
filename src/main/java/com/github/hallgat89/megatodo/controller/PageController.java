2e5e6b31f75be559b5c59e2dcf39c574ba17e668
Adam Hallgat <adam_hallgat@epam.com>
2022-09-14 17:18:23 +0200

    Add H2 and Liquibase

--- src/main/java/com/github/hallgat89/megatodo/controller/PageController.java
+++ src/main/java/com/github/hallgat89/megatodo/controller/PageController.java
@@ -1,8 +1,7 @@
-package com.github.hallgat89.megatodo.controllers;
+package com.github.hallgat89.megatodo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
---
+++ src/main/java/com/github/hallgat89/megatodo/domain/TodoEntity.java
@@ -0,0 +1,6 @@
+package com.github.hallgat89.megatodo.domain;
+
+public class TodoEntity {
+    Long id;
+    String description;
+}
