b3a255bf03bb525ad83dc7cd9b0aa032c62f1339
Adam Hallgat <adam_hallgat@epam.com>
2022-09-14 15:16:48 +0200

    Add Spring boot mvc

---
+++ src/main/java/com/github/hallgat89/SpringConfiguration.java
@@ -0,0 +1,9 @@
+package com.github.hallgat89;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.github.hallgat89")
+public class SpringConfiguration {
+}
