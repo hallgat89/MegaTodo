e28169abee1eba3415510b6940029e70fbfe2427
Adam Hallgat <adam_hallgat@epam.com>
2022-10-25 17:00:40 +0200

    Skip integration tests during build

--- src/test/java/com/github/hallgat89/megatodo/rest/RestTest.java
+++ src/test/java/com/github/hallgat89/megatodo/rest/RestTest.java
@@ -10,9 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 
-import java.time.LocalTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
