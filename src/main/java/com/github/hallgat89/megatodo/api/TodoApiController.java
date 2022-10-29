5b4666e27f80a0c61bfaa614573179e8581c0b18
Adam Hallgat <adam_hallgat@epam.com>
2022-10-29 21:52:15 +0200

    API decorated with descriptions

--- src/main/java/com/github/hallgat89/megatodo/api/TodoApiController.java
+++ src/main/java/com/github/hallgat89/megatodo/api/TodoApiController.java
@@ -2,15 +2,23 @@ package com.github.hallgat89.megatodo.api;
 
 import com.github.hallgat89.megatodo.service.TodoService;
 import com.github.hallgat89.megatodo.view.TodoView;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
+@Tag(name = "todoApi", description = "the TODO API")
 public class TodoApiController {
 
-    public static final String APIROOT = "/api";
     TodoService service;
 
     @Autowired
@@ -18,22 +26,35 @@ public class TodoApiController {
         this.service = service;
     }
 
-    @GetMapping(APIROOT + "/all")
+    @GetMapping("/all")
+    @Operation(summary = "Get all Todo Views", description = "Get all Todo Views")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(array = @ArraySchema(schema = @Schema(implementation = TodoView.class))))
+    })
     public List<TodoView> getAll() {
         return service.getAllTodos();
     }
 
-    @GetMapping(APIROOT + "/get")
+    @GetMapping("/get")
+    @Operation(summary = "Gets 1 Todo by ID", description = "Accepts an ID of an existing Todo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = TodoView.class)))
+    })
     public TodoView getById(@RequestParam(value = "id", required = true) Long id) {
         return service.getTodo(id);
     }
 
-    @PostMapping(APIROOT + "/new")
+    @PostMapping("/new")
+    @Operation(summary = "Creates a new todo from a message", description = "Accepts a String, returns an ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = Long.class)))
+    })
     public Long createTodo(@RequestParam(value = "message", required = true) String message) {
         return service.addNew(message);
     }
 
-    @DeleteMapping(APIROOT + "/delete")
+    @DeleteMapping("/delete")
+    @Operation(summary = "Deletes a todo", description = "Accepts an ID")
     public void deleteTodo(@RequestParam(value = "id", required = true) Long id) {
         service.delete(id);
     }
